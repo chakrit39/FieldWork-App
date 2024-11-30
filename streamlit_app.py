8eae804382d23e6bb93886d5bfeb72b48dc4c094
chakrit39 <126540185+chakrit39@users.noreply.github.com>
2024-11-30 16:06:48 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from streamlit_folium import folium_static
+#from streamlit_folium import folium_static
 import geopandas as gpd
 from requests.auth import HTTPBasicAuth
 import time
