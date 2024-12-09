0eaff9edb3cfb453b0e7080a8c62b25288eb3102
chakrit39 <126540185+chakrit39@users.noreply.github.com>
2024-12-10 00:33:22 +0700

    Update WorkSheet.py

--- WorkSheet.py
+++ WorkSheet.py
@@ -173,7 +173,7 @@ if chk1.checkbox("Upload a CSV file (Name,Code,N,E,H)"):
     if Point is not None:
         if Noneheader == True:
             data = pd.read_csv(Point,header=None)
-            data.rename(columns={"0": "Name", "1": "Code", "2": "N", "3": "E", "4": "H"}, inplace=True)
+            data = data.rename(columns={0: "Name", 1: "Code", 2: "N", 3: "E", 4: "H"})
         else:
             data = pd.read_csv(Point)
         st.dataframe(data=data,use_container_width=True)
