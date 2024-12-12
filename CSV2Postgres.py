4388e351aae6975c12ba393aabe320be49dc0016
chakrit39 <126540185+chakrit39@users.noreply.github.com>
2024-12-12 21:19:40 +0700

    Update CSV2Postgres.py

--- CSV2Postgres.py
+++ CSV2Postgres.py
@@ -23,26 +23,6 @@ scope = ['https://www.googleapis.com/auth/drive',
          'https://www.googleapis.com/auth/drive.file',
          'https://www.googleapis.com/auth/spreadsheets',
         ]
-
-@st.cache_resource 
-def get_service():
-    #if "creds" not in globals() :
-    creds = ServiceAccountCredentials.from_json_keyfile_dict(st.secrets["dol-mtd5-fieldwork"], scope)
-    gc = gspread.authorize(creds)
-    service = build("drive", "v3", credentials=creds)
-    sh = gc.open('องครักษ์')
-    wks = sh.worksheet('Raw')
-    sh_ref = gc.open('Report') 
-    wks_ref = sh_ref.worksheet('Ref')
-    sh_report = gc.open(Name+'-Report')
-    return creds,gc,service,sh,wks,sh_ref,wks_ref,sh_report
-            
-@st.cache_data
-def get_data():
-    df = pd.read_csv('./ONGKHARAK.csv',header=0)
-    sc = pd.read_csv('./UTMMAP4.csv',header=0,dtype={'UTMMAP4': str})
-    df_name = pd.read_csv("https://docs.google.com/spreadsheets/d/1taPadBX5zIlk80ZXc7Mn9fW-kK0VT-dgNfCcjRUskgQ/export?gid=0&format=csv",header=0)
-    return df,sc,df_name
     
 @st.dialog("สำเร็จ !!", width="small")
 def pop_up():
@@ -52,102 +32,24 @@ def pop_up():
         #st.session_state.vote = {"item": item, "reason": reason}
         st.rerun()    
 
-df,sc,df_name = get_data()
-
-st.title("แบบกรอกข้อมูลงานภาคสนาม สาขาองครักษ์")
+st.title("Upload CSV file to PostGIS")
     
-c01, c02, c03 = st.columns([0.35,0.35,0.3])
-parcel_no = c01.text_input("เลขที่โฉนด","")
-survey_no = c02.text_input("หน้าสำรวจ","")
-land_no = c03.text_input("เลขที่ดิน","")
-
-col_1, col_2, col_3, col_4, col_5 = st.columns([0.2,0.2,0.2,0.2,0.2])
-UTMMAP1 = col_1.text_input("UTMMAP1","")
-UTMMAP2 = col_2.selectbox("UTMMAP2",["I", "II", "III", "IV"],)
-UTMMAP3 = col_3.text_input("UTMMAP3","")
-Scale = col_4.selectbox("Scale",pd.unique(sc.SCALE),)
-UTMMAP4 = col_5.selectbox("UTMMAP4",pd.unique(sc.UTMMAP4[sc.SCALE==Scale]),)
-
-col1, col2, col3 = st.columns([0.35,0.35,0.3])
-province = col1.selectbox("จังหวัด",pd.unique(df.P_NAME_T))
-amphoe = col2.selectbox("อำเภอ",pd.unique(df.A_NAME_T[df.P_NAME_T==province]))
-tambon = col3.selectbox("ตำบล",pd.unique(df.T_NAME_T[df.A_NAME_T==amphoe]))
-
-"""
---------------
-"""
-cc1, cc2 = st.columns([0.5,0.5])
-BND_NAME = cc1.text_input("ชื่อหลักเขต","")
-Method = cc2.selectbox("เครื่องมือการรังวัด",["RTK GNSS","Total Station"])
-chk1, chk2 = st.columns([0.5,0.5])
-if chk1.checkbox("Upload a CSV file (Name,Code,N,E,H)"):
+if chk1.checkbox("Upload a CSV file (Name,Code,N,E,h)"):
     Noneheader = chk2.checkbox("None header")
     Point = st.file_uploader("เลือกไฟล์ CSV", accept_multiple_files=False, type=['csv'])
     if Point is not None:
         if Noneheader == True:
             data = pd.read_csv(Point,header=None)
-            data = data.rename(columns={0: "Name", 1: "Code", 2: "N", 3: "E", 4: "H"})
+            data = data.rename(columns={0: "Name", 1: "Code", 2: "N", 3: "E", 4: "h"})
         else:
             data = pd.read_csv(Point)
-        #st.dataframe(data=data['Code'].unique(),use_container_width=False)
-        if BND_NAME != "" :
-            data_point = data[['Code','N','E','H']][data.Code==BND_NAME]
-            data_point = data_point.reset_index(drop=True)
-            if len(data_point)==3:
-                #st.dataframe(data=data_point,use_container_width=False)
-                c1, c2, c3 = st.columns([0.4,0.4,0.2])
-                N1 = c1.text_input("N1",data_point.iloc[0,1])
-                N2 = c1.text_input("N2",data_point.iloc[1,1])
-                N3 = c1.text_input("N3",data_point.iloc[2,1])
 
-                E1 = c2.text_input("E1",data_point.iloc[0,2])
-                E2 = c2.text_input("E2",data_point.iloc[1,2])
-                E3 = c2.text_input("E3",data_point.iloc[2,2])
-
-                H1 = c3.text_input("H1",data_point.iloc[0,3])
-                H2 = c3.text_input("H2",data_point.iloc[1,3])
-                H3 = c3.text_input("H3",data_point.iloc[2,3])
-            elif len(data_point)==0:
-                st.warning("ไม่พบชื่อหมุดหลักเขต")
-            else:
-                st.warning("จำนวนค่าพิกัดหมุดหลักเขตไม่ครบหรือเกิน 3 ค่า")
-        else:
-            st.warning("โปรดใส่ชื่อหมุดหลักเขต")
-else:
-    c1, c2, c3 = st.columns([0.4,0.4,0.2])
-    N1 = c1.text_input("N1","")
-    N2 = c1.text_input("N2","")
-    N3 = c1.text_input("N3","")
-
-    E1 = c2.text_input("E1","")
-    E2 = c2.text_input("E2","")
-    E3 = c2.text_input("E3","")
-
-    H1 = c3.text_input("H1","")
-    H2 = c3.text_input("H2","")
-    H3 = c3.text_input("H3","")
-    
-"""
---------------
-"""
-
-image_1 = st.file_uploader("เลือกรูปขณะรับสัญญาณ", accept_multiple_files=False, type=['png', 'jpeg', 'jpg'],key=f"image_1-{st.session_state.uploader_key}")
-image_2 = st.file_uploader("เลือกรูปหมุดหลักเขต", accept_multiple_files=False, type=['png', 'jpeg', 'jpg'],key=f"image_2-{st.session_state.uploader_key}")
-image_3 = st.file_uploader("เลือกรูปตำแหน่งรับสัญญาณ", accept_multiple_files=False, type=['png', 'jpeg', 'jpg'],key=f"image_3-{st.session_state.uploader_key}")
 
 """
 -----------------
 """
 Name = st.selectbox("ผู้รังวัด",["ชาคฤตย์", "กิตติพันธุ์", "สุริยา", "ณัฐพร", "ศรัณย์", "ฐณิตา", "ปณิดา", "ปฐพี"],)
-f_name = df_name["F_Name-th"][df_name.Name==Name].iloc[0]
-l_name = df_name["L_Name-th"][df_name.Name==Name].iloc[0]
-full_name = "(" + f_name + "  " + l_name + ")"
-Sig = df_name["Signature"][df_name.Name==Name].iloc[0]
 
-date = st.date_input("วันที่ทำการรังวัด",format="DD/MM/YYYY")
-remark = st.text_input("หมายเหตุ","")
-
-creds,gc,service,sh,wks,sh_ref,wks_ref,sh_report = get_service()
 c001, c002 = st.columns([0.12,0.88])
 if c001.button("Submit"):
     #import time 
