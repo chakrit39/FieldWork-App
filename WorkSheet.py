6f11edabf263c58302e7ec5183fd660d0f0e907b
chakrit39 <126540185+chakrit39@users.noreply.github.com>
2024-12-15 10:40:16 +0700

    Update WorkSheet.py

--- WorkSheet.py
+++ WorkSheet.py
@@ -231,7 +231,31 @@ elif upload_method == "Import from PostGIS":
     sql = f'SELECT * FROM "public"."BND_Points"'
     gdf_postgis = gpd.GeoDataFrame.from_postgis(sql, engine, geom_col='geometry')
     gdf_postgis_new = gdf_postgis[['Name','Code','N','E','h','Remark']] #[gdf_postgis['ผู้รังวัด']==Name]
-    st.dataframe(data=gdf_postgis_new,use_container_width=True)
+    if BND_NAME != "" :
+        data_point = data[['Name','Code','N','E','h','Remark']][data.Code==BND_NAME]
+        data_point = data_point.reset_index(drop=True)
+        st.dataframe(data=data_point,use_container_width=True)
+        if len(data_point)==3:
+             #st.dataframe(data=data_point,use_container_width=False)
+             c1, c2, c3 = st.columns([0.4,0.4,0.2])
+             N1 = c1.text_input("N1",data_point.iloc[0,1])
+             N2 = c1.text_input("N2",data_point.iloc[1,1])
+             N3 = c1.text_input("N3",data_point.iloc[2,1])
+    
+             E1 = c2.text_input("E1",data_point.iloc[0,2])
+             E2 = c2.text_input("E2",data_point.iloc[1,2])
+             E3 = c2.text_input("E3",data_point.iloc[2,2])
+    
+             H1 = c3.text_input("H1",data_point.iloc[0,3])
+             H2 = c3.text_input("H2",data_point.iloc[1,3])
+             H3 = c3.text_input("H3",data_point.iloc[2,3])
+         elif len(data_point)==0:
+             st.warning("ไม่พบชื่อหมุดหลักเขต")
+         else:
+             st.warning("จำนวนค่าพิกัดหมุดหลักเขตไม่ครบหรือเกิน 3 ค่า")
+    else:
+        st.dataframe(data=gdf_postgis_new,use_container_width=True)
+        st.warning("โปรดใส่ชื่อหมุดหลักเขต")
 else:
     st.warning("โปรดเลือกวิธีนำเข้า")
 upload_method    
