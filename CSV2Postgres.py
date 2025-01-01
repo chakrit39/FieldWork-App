4df8230cc12fc8bfc7173466cfa9b5d5448be036
chakrit39 <126540185+chakrit39@users.noreply.github.com>
2025-01-01 23:02:13 +0700

    Update CSV2Postgres.py

--- CSV2Postgres.py
+++ CSV2Postgres.py
@@ -49,7 +49,7 @@ if Point is not None:
         
 sql = f'SELECT * FROM "public"."BND_Points"'        
 gdf_postgis = gpd.GeoDataFrame.from_postgis(sql, engine, geom_col='geometry')
-gdf_postgis.sort_values(by=['Index'])
+gdf_postgis = gdf_postgis.sort_values(by=['Index'])
 gdf_postgis = gdf_postgis.reset_index(drop=True)
 df = data
 gdf = gpd.GeoDataFrame(df,geometry=gpd.points_from_xy(df['E'],df['N']) , crs="EPSG:24047")
