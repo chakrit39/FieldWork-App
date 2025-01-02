8bde6719b62c32ecadc520e656f363b2010c17ac
chakrit39 <126540185+chakrit39@users.noreply.github.com>
2025-01-02 23:13:20 +0700

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -103,8 +103,9 @@ if len(gdf)!=0:
     gdf = gdf.set_geometry(gpd.points_from_xy(gdf.E,gdf.N),crs='EPSG:24047')
     gdf = gdf.to_crs('EPSG:4326')
     #st.dataframe(data=gdf)
-    lat = gdf.geometry.y
-    long = gdf.geometry.x
-    fo.CircleMarker([lat,long],radius = 3,color='#f56042',fill=True,fill_opacity=1).add_to(map)
-    #for lat,long
+    #lat = gdf.geometry.y
+    #lon = gdf.geometry.x
+    #fo.CircleMarker([lat,lon],radius = 3,color='#f56042',fill=True,fill_opacity=1).add_to(map)
+    for lat,lon in zip(gdf.geometry.y,gdf.geometry.x)
+        fo.CircleMarker([lat,lon],radius = 3,color='#f56042',fill=True,fill_opacity=1).add_to(map)
 folium_static(map)
