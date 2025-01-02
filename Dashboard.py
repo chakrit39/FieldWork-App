fb86a01c6a10c3fff3118f07c2f86ef4f33882ad
chakrit39 <126540185+chakrit39@users.noreply.github.com>
2025-01-03 00:33:06 +0700

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -104,6 +104,7 @@ if len(gdf)!=0:
     if Name != "ทั้งหมด":
         gdf = gdf[gdf['ผู้รังวัด']==Name]
     gdf = gdf.set_geometry(gpd.points_from_xy(gdf.E,gdf.N),crs='EPSG:24047')
+    st.dataframe(data=gdf)
     gdf = gdf.to_crs(4326)
     st.dataframe(data=gdf)
     gdf.crs
