ffb60b17c66258bd42420e1f5bb3e6f02d3fe793
chakrit39 <126540185+chakrit39@users.noreply.github.com>
2025-01-03 00:43:51 +0700

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -105,7 +105,7 @@ if len(gdf)!=0:
         gdf = gdf[gdf['ผู้รังวัด']==Name]
     gdf = gdf.set_geometry(gpd.points_from_xy(gdf.E,gdf.N),crs='EPSG:24047')
     #st.dataframe(data=gdf)
-    gdf = gdf.to_crs(4326)
+    gdf = gdf.to_crs(epsg=4326)
     #st.dataframe(data=gdf)
     #gdf.crs
     #lat = gdf.geometry.y
