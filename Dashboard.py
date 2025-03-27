8d4ff5ac4389b3d6beefb1e713340b0d8651d8b7
chakrit39 <126540185+chakrit39@users.noreply.github.com>
2025-03-27 14:17:17 +0700

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -112,11 +112,10 @@ st.dataframe(
 st.header('Map')
 map = get_map()
 
-Name_list = df["Name"].to_list()
-Name_list = Name_list[0:len(Name_list)-1]
-Name_list
-Name_list = ["ทั้งหมด"].append(Name_list)
-Name_list
+Name_list_ = df["Name"].to_list()
+Name_list_ = Name_list[0:len(Name_list)-1]
+Name_list = ["ทั้งหมด"]
+Name_list = Name_list.append(Name_list)
 Name = st.selectbox("ผู้รังวัด",Name_list,on_change=get_Refresh())
 if st.button("Refresh"):
     st.session_state["Refresh"] = True
