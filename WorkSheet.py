326b5a5405bbcd0f6ff35656d95c7825b9e5dec3
chakrit39 <126540185+chakrit39@users.noreply.github.com>
2025-05-14 11:17:40 +0700

    Update WorkSheet.py

--- WorkSheet.py
+++ WorkSheet.py
@@ -170,12 +170,12 @@ def pop_up():
         #st.session_state.vote = {"item": item, "reason": reason}
         st.rerun()    
         
-Office = ['องครักษ์','ลำลูกกา']        
+Office = ['องครักษ์','ลำลูกกา','ธัญบุรี','คลองหลวง']        
 placeholder = st.empty()
 with placeholder.form("login"):
     st.markdown("#### โปรดเลือก")
     office_select = st.selectbox("สำนักงานที่ดิน",Office)
-    round_ = st.selectbox("รอบที่",["1","2"])
+    round_ = st.selectbox("รอบที่",["1","2","3"])
     Login = st.form_submit_button("Login")
     if Login:
         st.session_state["Login"] = True
