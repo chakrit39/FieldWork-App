130cd5ad5de56d24a69158284a4e75f9494a0c7e
chakrit39 <126540185+chakrit39@users.noreply.github.com>
2025-10-31 12:28:20 +0700

    Update Query.py

--- Query.py
+++ Query.py
@@ -130,3 +130,7 @@ st.pyplot(fig)
 
 data_point = gpd.read_file(point_url)[['PCM_BNDNAME' , 'PCM_NORTH' , 'PCM_EAST']]
 st.dataframe(data=data_point,use_container_width=True)
+"""
+    --------------
+"""
+
