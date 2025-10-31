92fd9b82da2d7269b658ce8ca6a6baba41ac4025
chakrit39 <126540185+chakrit39@users.noreply.github.com>
2025-10-31 13:14:04 +0700

    Update Query.py

--- Query.py
+++ Query.py
@@ -53,19 +53,18 @@ UTMMAP4 = col_5.selectbox("UTMMAP4",pd.unique(sc.UTMMAP4[sc.SCALE==Scale]),)
 land_no = col_6.text_input("เลขที่ดิน","")
 
 if st.button("Search"):
-    #import time 
-    #start = time.time()
     st.session_state["Search"] = True
-else:
-    st.session_state["Search"] = False
-
-if st.session_state["Search"] ==  True:
-    # === Path ไปยังไฟล์ของคุณ ===
     poly_url = "https://drive.google.com/uc?id=1T731fgDUaa-DcRHHirZiv165JMy2rIfg&export%3Fformat=geojson"
     point_url = "https://drive.google.com/uc?id=1cHJhf_gicoUIekg1MqKk3WCDY65CmGGt&export%3Fformat=geojson"
     
     # === โหลดไฟล์ ===
     poly_data,point_data,data_point = get_data()
+else:
+    st.session_state["Search"] = True
+
+if st.session_state["Search"] ==  True:
+    # === Path ไปยังไฟล์ของคุณ ===
+
     
     polygons = [shape(feat["geometry"]) for feat in poly_data["features"]]
     points = [shape(feat["geometry"]) for feat in point_data["features"]]
