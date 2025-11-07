7b323d41199216bf920a9165a8df772f0c176127
chakrit39 <126540185+chakrit39@users.noreply.github.com>
2025-11-07 15:24:30 +0700

    Update WorkSheet.py

--- WorkSheet.py
+++ WorkSheet.py
@@ -245,40 +245,6 @@ if st.session_state["Login"]:
                     st.warning("จำนวนค่าพิกัดหมุดหลักเขตไม่ครบหรือเกิน 3 ค่า")
             else:
                 st.warning("โปรดใส่ชื่อหมุดหลักเขต")
-    elif upload_method == "Upload a CSV file (PostGIS)":
-        chk2.write("")
-        chk2.write("")
-        Noneheader = chk1.checkbox("None header")
-        Point = st.file_uploader("เลือกไฟล์ CSV", accept_multiple_files=False, type=['csv'])
-        if Point is not None:
-            if Noneheader == True:
-                data = pd.read_csv(Point,header=None)
-                data = data.rename(columns={0: "Name", 1: "Code", 2: "N", 3: "E", 4: "h"})
-            else:
-                data = pd.read_csv(Point)
-            #st.dataframe(data=data['Code'].unique(),use_container_width=False)
-            if BND_NAME != "" :
-                data_point = data[['NAME','CODE','N','E','H','REMARK','DATE']][data.CODE==BND_NAME]
-                data_point = data_point.reset_index(drop=True)
-                if len(data_point)==3:
-                    st.dataframe(data=data_point,width="stretch")
-                    c1, c2, c3 = st.columns([0.4,0.4,0.2])
-                    N1 = c1.text_input("N1",data_point.iloc[0,2])
-                    N2 = c1.text_input("N2",data_point.iloc[1,2])
-                    N3 = c1.text_input("N3",data_point.iloc[2,2])
-                    E1 = c2.text_input("E1",data_point.iloc[0,3])
-                    E2 = c2.text_input("E2",data_point.iloc[1,3])
-                    E3 = c2.text_input("E3",data_point.iloc[2,3])
-                    H1 = c3.text_input("H1",data_point.iloc[0,4])
-                    H2 = c3.text_input("H2",data_point.iloc[1,4])
-                    H3 = c3.text_input("H3",data_point.iloc[2,4])
-                elif len(data_point)==0:
-                    st.warning("ไม่พบชื่อหมุดหลักเขต")
-                else:
-                    st.dataframe(data=data_point,width="stretch")
-                    st.warning("จำนวนค่าพิกัดหมุดหลักเขตไม่ครบหรือเกิน 3 ค่า")
-            else:
-                st.warning("โปรดใส่ชื่อหมุดหลักเขต")
     elif upload_method == "Import from PostGIS":
         office_ = pd.DataFrame([["องครักษ์","ONGKHARAK"],["ลำลูกกา","LUMLUKKA"],["ธัญบุรี","THANYABURI"],["คลองหลวง","KHLONGLUANG"],["ปทุมธานี","PATHUMTHANI"],["นครนายก","NAKHONNAYOK"]],columns=["th","eng"])
         office_choice = office_['eng'][office_['th']==office_select].iloc[0]
