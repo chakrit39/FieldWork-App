2a9629c1f9aec3533f7ca7809bc838177ecb5c34
chakrit39 <126540185+chakrit39@users.noreply.github.com>
2025-11-10 19:47:01 +0700

    Update Query.py

--- Query.py
+++ Query.py
@@ -105,7 +105,10 @@ if st.session_state["verity"]:
     Scale = col_4.selectbox("Scale",pd.unique(sc.SCALE),)
     UTMMAP4 = col_5.selectbox("UTMMAP4",pd.unique(sc.UTMMAP4[sc.SCALE==Scale]),)
     land_no = col_6.text_input("เลขที่ดิน","")
+
+    UTM_Name 
     st.session_state
+    
     if st.button("Search"):
         if UTMMAP1 != "" and UTMMAP3 != "" and land_no != "" :
                 
