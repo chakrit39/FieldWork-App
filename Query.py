4a6b6e8f5d43067fd081e85999c917ac8e7554b0
chakrit39 <126540185+chakrit39@users.noreply.github.com>
2025-11-11 15:25:20 +0700

    Update Query.py

--- Query.py
+++ Query.py
@@ -23,8 +23,8 @@ import requests
 import matplotlib.font_manager as fm
 import uuid
 from streamlit_cookies_manager import EncryptedCookieManager
-cookies = EncryptedCookieManager()
-cookies
+cookies = EncryptedCookieManager(prefix="my_app",password="my_secrets_key")
+st.write(cookies)
 #st.write(str(uuid.uuid4()))
 #query_params = st.query_params
 #if "user_id" not in query_params :
