523b7cdda57507b24ae7e601b76e13c238618ca3
chakrit39 <126540185+chakrit39@users.noreply.github.com>
2025-11-11 15:48:44 +0700

    Update Query.py

--- Query.py
+++ Query.py
@@ -21,26 +21,16 @@ from shapely.geometry import shape, Point
 import math
 import requests
 import matplotlib.font_manager as fm
-import uuid
+
 from streamlit_cookies_manager import EncryptedCookieManager
 cookies = EncryptedCookieManager(prefix="my_app",password="my_secrets_key")
-cookies.__dict__
-cookies._cookie_manager["session_id"]
-st.session_state
-#if not cookies.ready():
-#    st.stop()
+cookies = cookies._cookie_manager["session_id"]
 if "cookies" not in st.session_state:
     st.session_state["cookies"] = {}
-#cookies["session_id"]
-#st.write(str(uuid.uuid4()))
-#query_params = st.query_params
-#if "user_id" not in query_params :
-#    user_id = str(uuid.uuid4())
-#    st.query_params[user_id] = 1
-#else:
-#    user_id = query_params["user_id"][0]
-#st.query_params
-#st.session_state
+if cookies not in st.session_state["cookies"]:
+    st.session_state["cookies"][cookies] = {}
+st.session_state
+
 if "Search" not in st.session_state:
     st.session_state["Search"] = False
     
