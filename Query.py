c6cebdac9bd95947bde9b7845b224af8359dabb2
chakrit39 <126540185+chakrit39@users.noreply.github.com>
2025-11-11 15:28:02 +0700

    Update Query.py

--- Query.py
+++ Query.py
@@ -24,11 +24,11 @@ import matplotlib.font_manager as fm
 import uuid
 from streamlit_cookies_manager import EncryptedCookieManager
 cookies = EncryptedCookieManager(prefix="my_app",password="my_secrets_key")
+cookies.__dict__
 if not cookies.ready():
     st.stop()
 if "session_id" not in cookies:
     cookies["session_id"] = str(uuid.uuid4())
-st.write(cookies)
 cookies["session_id"]
 #st.write(str(uuid.uuid4()))
 #query_params = st.query_params
