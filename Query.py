de4d635de519bd81e25bb1339e3a38441b409bd0
chakrit39 <126540185+chakrit39@users.noreply.github.com>
2025-11-12 15:13:48 +0700

    Update Query.py

--- Query.py
+++ Query.py
@@ -60,9 +60,9 @@ if "cookies" not in st.session_state:
 # store cookie-specific dict server-side (avoids direct low-level api usage)
 if session_cookie_id not in st.session_state["cookies"]:
     st.session_state["cookies"][session_cookie_id] = {}
-if "Data" not in cookie_manager:
-    cookie_manager.set("session_id", {})
-    cookie_manager.save()
+#if "Data" not in cookie_manager:
+#    cookie_manager.set("session_id", {})
+#    cookie_manager.save()
 cookie_manager
 st.session_state
 scope = ['https://www.googleapis.com/auth/drive',
