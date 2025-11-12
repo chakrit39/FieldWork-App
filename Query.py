5602e465d8ca13542049d245efa798f661cd29ce
chakrit39 <126540185+chakrit39@users.noreply.github.com>
2025-11-12 11:06:24 +0700

    Update Query.py

--- Query.py
+++ Query.py
@@ -140,10 +140,10 @@ if st.session_state["verity"]:
             # === Path ไปยังไฟล์ของคุณ ===
             UTM = str(UTMMAP1) + " " + str(UTMMAP2) + " " + str(UTMMAP3) + "-" + str(UTMMAP4) + "(" + str(Scale) + ")_" + str(land_no)
             UTM_Name = get_UTM_Name(UTM)
-            if "UTM_Name" in st.session_state["cookies"][cookies]:
-                if st.session_state["cookies"][cookies]["UTM_Name"] != UTM:
-                    get_UTM_Name.clear()
-                    UTM_Name = get_UTM_Name(UTM)
+
+            if st.session_state["cookies"][cookies]["UTM_Name"] != UTM:
+                get_UTM_Name.clear()
+                UTM_Name = get_UTM_Name(UTM)
             
             id = df[df['Name']==UTM]
             if len(id) == 0 :
