458b7fd1aedf3b59a4359f8256b453ee89ccf6cd
chakrit39 <126540185+chakrit39@users.noreply.github.com>
2025-11-12 11:15:30 +0700

    Update Query.py

--- Query.py
+++ Query.py
@@ -167,11 +167,11 @@ if st.session_state["verity"]:
             --------------
     """
     if "cookies" in st.session_state:
-        print("cookies")
+        st.write("cookies")
         if  cookies in st.session_state["cookies"] :
-            print(cookies)
+            st.write(cookies)
             if "UTM_Name" in st.session_state["cookies"][cookies]:
-                print("UTM_Name")
+                st.write("UTM_Name")
                 if st.session_state["cookies"][cookies]["UTM_Name"] in st.session_state["Data"] :
                     UTM_Name_ = st.session_state["cookies"][cookies]["UTM_Name"]
                     poly_data = st.session_state["Data"][UTM_Name_]["poly_data"]
