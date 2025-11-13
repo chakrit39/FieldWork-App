c55ebb96a55ac92e34918737f1d9e1b5a975ae16
chakrit39 <126540185+chakrit39@users.noreply.github.com>
2025-11-13 09:11:22 +0700

    Update Query.py

--- Query.py
+++ Query.py
@@ -114,7 +114,7 @@ if st.session_state["verity"] == False:
         
 creds,gc,service,sh,wks = get_service()
 df,sc = get_List()  
-st.wrtie(str(uuid.uuid4()))
+st.write(str(uuid.uuid4()))
 cookie_manager
 
 if st.session_state["verity"]:
