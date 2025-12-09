5b7aea3fd82aef7e5d26a02f0d63416cf64635ad
chakrit39 <126540185+chakrit39@users.noreply.github.com>
2025-12-09 22:51:34 +0700

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -198,7 +198,8 @@ with tab2:
     df_name_['จำนวนแปลง'] = 0
     df_name_['จำนวนหมุด'] = 0
     for j in range(len(df_name_)):
-        gdf_L2_temp = gdf_L2[gdf_L2["CODE_N"]==df_name_["ลำดับ"][j]]
+        k = df_name_["ลำดับ"][j]
+        gdf_L2_temp = gdf_L2[gdf_L2["CODE_N"]==k]
         df_name_['จำนวนแปลง'][j] = len(gdf_L2_temp)
         gdf_BND_temp = gdf_BND[gdf_BND["Surveyer"]==df_name_["Name"][j]]
         df_name_['จำนวนหมุด'][j] = round((len(gdf_BND_temp)/3)-0.5,0)    
